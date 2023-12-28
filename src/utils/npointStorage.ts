5eb68355a217e9a3fe07c3d7ced7af7a5352cfa1
Yasin <yasinntazeoglu@gmail.com>
2023-12-28 06:48:44 +0300

    updates

--- src/utils/npointStorage.ts
+++ src/utils/npointStorage.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export function npointStorage(id: string) {
   const apiUrl = `https://api.npoint.io/${id}`;
@@ -12,7 +13,9 @@ export function npointStorage(id: string) {
   };
   return {
     getItem: () => fetch(apiUrl).then((res) => res.text()),
+    // @ts-ignore
     setItem: (name: string, val: string) => set(val),
+    // @ts-ignore
     removeItem: async (name: string) => set('{}'),
   };
 }
