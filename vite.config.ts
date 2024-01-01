64e2f5768129c1d054b3132cbde42ca1ea32816e
Yasin <yasinntazeoglu@gmail.com>
2024-01-01 05:01:02 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-import styleX from 'vite-plugin-stylex';
-
 export default defineConfig(({ mode }) => {
   return {
     plugins: [
       tsconfigPaths(),
       react(),
-      styleX(),
       mode === 'analyze' && visualizer({ open: true }),
     ],
     base: '/mesai-takip',
