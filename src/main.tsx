159d6bb99f31916a95e6ae513ef56100dd6007ac
Yasin <yasinntazeoglu@gmail.com>
2024-01-01 04:50:21 +0300

    v1

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import Modal from 'react-modal';
 import { router } from './routes.tsx';
 
 import 'react-spring-bottom-sheet/dist/style.css';
 import './app.scss';
-
-Modal.setAppElement('#modal-root');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 import styleX from 'vite-plugin-stylex';
 
-export default defineConfig({
-  plugins: [tsconfigPaths(), react(), styleX()],
-  base: '/mesai-takip',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      styleX(),
+      mode === 'analyze' && visualizer({ open: true }),
+    ],
+    base: '/mesai-takip',
+  };
 });
